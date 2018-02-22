2ccbd3dfff4c2ba42659a4e9423f1220a34f013a
Arpita Nema <nemaarpita2012@gmail.com>
2018-02-22 07:36:00 +0530

    Add files via upload

---
+++ student/index.php
@@ -0,0 +1,12 @@
+<?php 
+	include_once "..\header.php";
+	//include_once "header.php";
+	//require_once "header.php";
+
+?>
+<h2>STUDENT</h2>
+coming soon.....
+
+<?php 
+	require "../footer.php";
+?>
